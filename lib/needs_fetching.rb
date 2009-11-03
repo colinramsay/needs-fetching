456087bd4fc2269f59590d422560f77c5fb7bcbf
Colin <colin@colin-ramsays-macbook-pro.local>
2009-11-03 11:15:40 +0000

    passing specs

--- lib/needs_fetching.rb
+++ lib/needs_fetching.rb
@@ -1,7 +1,29 @@
+require 'ruby-debug'
 require 'open-uri'
 
 module OpenURI::Meta
   def needs_fetching?(last_retrieved, last_etag)
-    false
+    
+    expires = self.meta['expires']
+    etag = self.meta['etag']
+    last_modified = self.meta['last-modified']
+
+    if(last_modified == nil and etag == nil and expires == nil)
+      return true
+    end
+
+    if(etag && etag == last_etag)
+      return false
+    end
+
+    if(expires and Date.parse(expires) >= Date.today)
+      return false
+    end
+    
+    if(last_modified and Date.parse(last_modified) < last_retrieved)
+      return false
+    end
+    
+    return true
   end
 end
--- spec/lib/needs_fetching_spec.rb
+++ spec/lib/needs_fetching_spec.rb
@@ -5,22 +5,48 @@ describe OpenURI::Meta do
   
   before do
     @url = "http://example.com/test1"
-    FakeWeb.register_uri(:get, @url, :body => "Hello World!")
-    @result = open(@url)
   end
   
   it "will return true if no past data provided" do
-    @result.needs_fetching?(nil, nil).should be_false
+    FakeWeb.register_uri(:get, @url, :body => "Hello World!")
+    @result = open(@url)
+    @result.needs_fetching?(nil, nil).should be_true
   end
   
-  it "will return true if expires has passed"
+  it "will return true if expires has passed" do
+    FakeWeb.register_uri(:get, @url, :body => "Hello World!", :expires => 'Thu, 01 Dec 1994 16:00:00 GMT')
+    @result = open(@url)
+    @result.needs_fetching?(nil, nil).should be_true
+  end
   
-  it "will return true if last modified is newer than last fetched"
+  it "will return true if last modified is newer than last fetched" do
+     FakeWeb.register_uri(:get, @url, :body => "Hello World!", :last_modified => 'Thu, 01 Dec 2995 16:00:00 GMT')
+      @result = open(@url)
+      @result.needs_fetching?(Date.parse('Thu, 01 Dec 1994 13:00:00 GMT'), nil).should be_true
+  end
   
-  it "will return false if etag matches"
+  it "will return true if last modified is not available" do
+    FakeWeb.register_uri(:get, @url, :body => "Hello World!")
+    @result = open(@url)
+    @result.needs_fetching?(Date.parse('Thu, 01 Dec 1994 13:00:00 GMT'), 'blablabla').should be_true
+  end
   
-  it "will return false if last modified is older than last fetched"
+  it "will return false if etag matches" do
+    FakeWeb.register_uri(:get, @url, :body => "Hello World!", :etag => 'blablabla')
+    @result = open(@url)
+    @result.needs_fetching?(Date.parse('Thu, 01 Dec 1994 13:00:00 GMT'), 'blablabla').should be_false
+  end
   
-  it "will return true if last modified is not provided"
+  it "will return false if last modified is older than last fetched" do
+    FakeWeb.register_uri(:get, @url, :body => "Hello World!", :last_modified => 'Thu, 01 Dec 1995 16:00:00 GMT')
+    @result = open(@url)
+    @result.needs_fetching?(Date.parse('Thu, 01 Dec 1996 13:00:00 GMT'), nil).should be_false
+  end
+
+  it "will return false if expires has not passed" do
+    FakeWeb.register_uri(:get, @url, :body => "Hello World!", :expires => 'Thu, 01 Dec 2994 16:00:00 GMT')
+    @result = open(@url)
+    @result.needs_fetching?(Date.parse('Thu, 01 Dec 1994 13:00:00 GMT'), nil).should be_false
+  end
   
 end
